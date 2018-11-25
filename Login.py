677cfa099bb71ed2b9a03bcd75b65d3d9572fe4c
Elhanan89 <Bsnani7@gmail.com>
2018-11-25 15:36:25 +0200

    update

--- Login.py
+++ Login.py
@@ -1,2 +1,27 @@
+import Work
 
-f = open("try.txt")
+flag = False
+username=input("Enter your User Name: \n")
+Password = input("Enter Password: \n")
+
+def ReadDict():
+    # Dict that will contain keys and values
+    dictionary = {}
+    with open("try.txt", "rt") as file:
+        for line in file:
+            s = line.strip().split(", ")
+            dictionary[s[0]] = int(s[1])
+
+    if username in dictionary:
+        if dictionary[username]==Password:
+            print("Login Seccess")
+
+
+    return dictionary
+
+
+
+
+
+
+
