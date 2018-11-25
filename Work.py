cdd4ae1b5f74f73009fd04bf5323a056e7eb8b4e
matan lazimi <MatanLazimi@gmail.com>
2018-11-25 13:51:10 +0200

    1.1

--- Work.py
+++ Work.py
@@ -1,22 +1,13 @@
 import calendar
 from datetime import datetime
 
+global a
 now=datetime.now()
 print(calendar.calendar(now.year))
 
 #waiter menu
-print('to choose your days you want to work press 1')
+print('to choose your days you want to work press 1')#1.2
 print('to see your work schedule press 2')
 print('to see your personal information press 3')#in this fuction need to another button to update the personal information
 print('to aaaa')
 
-
-global a
-a=list()
-def Insert_days_to_work():
-    global a
-    print('Welcome to page of choosing days for working')
-    a=list(input("press days that you want to work: "))
-
-Insert_days_to_work()
-print(a)
