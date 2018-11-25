afd9a5cb2f935b483686ea31bf773883b70b9ea1
matan lazimi <MatanLazimi@gmail.com>
2018-11-25 15:06:40 +0200

    add a global variable to 1.8

--- Work.py
+++ Work.py
@@ -2,6 +2,11 @@ import calendar
 from datetime import datetime
 
 global a
+global event,feedback_waiter
+feedback_waiter={}
+event={"22/12/2018":"Festival Can"}
+
+
 now=datetime.now()
 print(calendar.calendar(now.year))
 
@@ -10,4 +15,3 @@ print('to choose your days you want to work press 1')#1.2
 print('to see your work schedule press 2')
 print('to see your personal information press 3')#in this fuction need to another button to update the personal information
 print('to aaaa')
-
