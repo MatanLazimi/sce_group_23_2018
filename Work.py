930745433b6ca119e24100019a6eb2a4c0c53f9e
matan lazimi <MatanLazimi@gmail.com>
2018-11-25 12:58:47 +0200

    12121

--- Work.py
+++ Work.py
@@ -9,3 +9,14 @@ print('to choose your days you want to work press 1')
 print('to see your work schedule press 2')
 print('to see your personal information press 3')#in this fuction need to another button to update the personal information
 print('to ')
+
+
+global a
+a=list()
+def Insert_days_to_work():
+    global a
+    print('Welcome to page of choosing days for working')
+    a=list(input("press days that you want to work: "))
+
+Insert_days_to_work()
+print(a)
