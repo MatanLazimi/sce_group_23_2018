5aa83872d7d01f80dbf94aaa6a73e9465b94596d
יצחק מימון <tzahy92@gmail.com>
2018-11-25 13:40:41 +0200

    tzahy

--- wiater.py
+++ wiater.py
@@ -8,3 +8,8 @@ print(calendar.calendar(now.year))
 print('to choose your days you want to work press 1')
 print('to see your work schedule press 2')
 print('to see your personal information press 3')#in this fuction need to another button to update the personal information
+print('to send feedback on event press 4')
+print('to see the report shifts of the last month press 5')
+print('to send application for change the work schedule press 6')
+print('to send general application to manager or responsible shift 7')
+
