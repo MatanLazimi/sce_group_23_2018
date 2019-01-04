bf85204c38077dcbb477d076c29c3e8f949292d1
matan lazimi <MatanLazimi@gmail.com>
2019-01-04 15:57:34 +0200

    2.7

--- ahmash.py
+++ ahmash.py
@@ -1,4 +1,11 @@
 #(2.7)
 global request
 def request_manpower():
-seff=fghfh
+    request={}
+    print("For getting more waiters to the relvant shift, press the date the needed dd/mm/yyyy")
+    date_req=input("")
+    amount=input("Insert the amount of waiters that you need: ")
+    request[date_req]=amount
+
+request_manpower()
+print(request)
