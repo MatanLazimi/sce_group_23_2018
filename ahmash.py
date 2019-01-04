8a0d85ff5927a351b4e8f637c9f792394108957b
matan lazimi <MatanLazimi@gmail.com>
2019-01-04 16:50:43 +0200

    2.11

--- ahmash.py
+++ ahmash.py
@@ -15,12 +15,17 @@ print(request)
 #matan
 #(2.11)
 def request_from_manager():
-    print("For request from manager first insert your details:")
+    print("For request from manager first insert your details-")
     FirstName = str(input('Enter your first name: '))
     LastName = str(input('Enter your last name: '))
     ID = int(input('Enter your ID: '))
     print("Insert a request for your manager: (when you finish press enter)")
     request=input()
-    request_manager = {'FirstName': FirstName, 'LastName': LastName, 'ID': ID, request}
-
-
+    request_manager['FirstName']=FirstName
+    request_manager['LastName']=LastName
+    request_manager['ID']= ID
+    request_manager['request']=request
+"""
+request_from_manager()
+print(request_manager)
+"""
