dfad103e4e22375b5ad76fd2df08cc433e3c3075
matan lazimi <MatanLazimi@gmail.com>
2019-01-04 16:25:18 +0200

    2.11

--- ahmash.py
+++ ahmash.py
@@ -1,6 +1,7 @@
 #(2.7)
-global request
+global request, request_manager
 request = {}
+request_manager= {}
 def request_manpower():
     print("For getting more waiters to the relvant shift, press the relevant date dd/mm/yyyy")
     date_req=input("")
@@ -11,4 +12,14 @@ def request_manpower():
 request_manpower()
 print(request)
 """
+#matan
+#(2.11)
+def request_from_manager():
+    print("For request from manager first insert your details:")
+    FirstName = str(input('Enter your first name: '))
+    LastName = str(input('Enter your last name: '))
+    ID = int(input('Enter your ID: '))
+    print("Insert a request for your manager: (when you finish press enter)")
+    request=input()
+    request_manager = {'FirstName': FirstName, 'LastName': LastName, 'ID': ID, request}
 
