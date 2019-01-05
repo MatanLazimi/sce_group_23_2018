78e842396f2016fdccbcd69d6e2e4ab7ca20cf2a
matan lazimi <MatanLazimi@gmail.com>
2019-01-05 10:14:31 +0200

    testing

--- myTest.py
+++ myTest.py
@@ -2,13 +2,12 @@ import unittest
 import waiter
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        #self.assertEqual(True, False)
-        #self.assertEqual(waiter.PersonalInformation(),False)
+    def test_ReadDict(self):
+        self.assertEqual(manager('Matan_La', 1234), True)
 
     def test_PersonalInformation(self):
         #self.assertRaises(ValueError,waiter.PersonalInformation(),)
-
+        pass
 
 if __name__ == '__main__':
    unittest.main()
